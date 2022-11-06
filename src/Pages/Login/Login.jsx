c2decd0b6d40781b4838c4d54105b056f02cfe37
hasibul240 <hasibul112002@gmail.com>
2022-11-06 16:18:07 +0600

    jwt

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -21,7 +21,22 @@ const Login = () => {
             .then(result => {
                 const user = result.user
                 console.log(user);
-                navigate(from, { replace: true });
+
+                const current_user = {
+                    email: user.email,
+                }
+
+                fetch(`http://localhost:5000/jwt`, {
+                    method: 'POST',
+                    headers: { 'content-type': 'application/json' },
+                    body: JSON.stringify({ current_user })
+                })
+                    .then(res => res.json())
+                    .then(data => { 
+                        console.log(data)
+                    })
+
+                // navigate(from, { replace: true });
             })
             .catch(error => console.error(error));
 
